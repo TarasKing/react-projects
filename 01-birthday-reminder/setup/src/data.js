158bb7e56c961fe28b9a5c3cd4b34cff865d959d
TarasKing <ivanovtaras@live.ru>
2022-07-02 13:55:10 -0700

    updated

--- 01-birthday-reminder/setup/src/data.js
+++ 01-birthday-reminder/setup/src/data.js
@@ -34,4 +34,11 @@ export default [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
+    {
+    id: 6,
+    name: 'Taras',
+    age: 34,
+    image:
+      'https://scontent.fyvr4-1.fna.fbcdn.net/v/t31.18172-8/26758134_1560574554032551_1960137763799579319_o.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=cJNdNbIYOXEAX-De4kA&_nc_ht=scontent.fyvr4-1.fna&oh=00_AT8uA5p9LmjOrNP0MqGxIDz4BrvPZ5qAZpdbuhujhMWsmA&oe=62979452',
+  },
 ];
